c8f85b02bbb5ad1615a969d067a2683054be85eb
amine <174889557+aminato-sama@users.noreply.github.com>
2024-07-06 17:41:35 +0200

    Ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+    var res = n >= 0 ? "":"ERROR";
 
+    for(var i = 0; i < n; i++){
+        res+= s;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
