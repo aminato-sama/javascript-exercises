6fb8bbf88c08d76c6fd749e79f9983ecbbb4a507
amine <174889557+aminato-sama@users.noreply.github.com>
2024-07-06 17:50:13 +0200

    Ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var returnValue = "";
 
+    for(var i = string.length - 1; i >= 0; i--){
+        returnValue+= string[i];
+    }
+
+    return returnValue;
 };
 
 // Do not edit below this line
