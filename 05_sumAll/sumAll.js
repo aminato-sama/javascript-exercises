a211ab281405a17d27af91283a97a5b2be6b1162
amine <174889557+aminato-sama@users.noreply.github.com>
2024-07-09 08:51:01 +0200

    Ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    if(a < 0 || b < 0 || 
+        typeof(a) != "number" || typeof(b) != "number"){
+            return "ERROR";
+        }
+    
+    var sum = 0;
+    var c = Math.min(a, b);
+    var d = Math.max(a, b);
 
+    for(var i = c; i <= d; i++){
+        sum+= i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
